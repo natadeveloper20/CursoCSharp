2f8659d67e6b6d59134bc0a580fa00827d61a63d
Natanael Mendieta <mendietanatanael9@gmail.com>
2026-06-30 21:06:59 -0300

    //Avanzamos con el tema de condicionales, en este caso con un ejemplo de if y else, para determinar si una persona es mayor de edad o no.

--- 2Condicionales/Program.cs
+++ 2Condicionales/Program.cs
@@ -44,3 +44,32 @@ else if (edadPersona1 == edadPersona2)
     Console.WriteLine("La persona 1 es igual a la persona 2");
 }
 
+// Vamos con un ejemplo con string
+
+string sexoPersona1 = "Hombre";
+
+if ((edadPersona1 > edadPersona2) && (sexoPersona1 == "Hombre"))
+{
+    Console.WriteLine("La persona 1 es mayor que la persona 2 y es hombre");
+}
+else if ((edadPersona1 < edadPersona2) || (sexoPersona1 == "Hombre"))
+{
+    Console.WriteLine("La persona 1 es menor que la persona 2 y es hombre");
+}
+
+// Vamos con otro ejercicio 
+
+int x = 10;
+int y = 5;
+int z = 3;
+
+// Queremos que se cumpla: 
+// (x > 5 && y < 10 && z == 3))
+if (x > 5 && y < 10 && z == 3)
+{
+    Console.WriteLine("Se cumple la condición");
+}
+else
+{
+    Console.WriteLine("No se cumple la condición");
+}
